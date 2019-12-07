b903726564dbd95f23c23d48cef531b6b5332f4e
Jake Waffle <jacqueswafel@gmail.com>
2019-12-06 21:25:00 -0800

    Removed README from the setup.py file.

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup
-import os 
-
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-
-with open(os.path.join(ROOT_PATH, "README"), 'r') as f:
-    long_description = f.read()
 
 setup(
    name='pycope',
    version='1.0',
    license="Apache2",
-   description='Tools for Context-Oriented Programming.',
+   description='A Context-Oriented Programming Extension for Python.',
    author='Jacob Waffle',
    author_email='thejakewaffle@gmail.com',
    url="http://www.github.com/pycope",
-   long_description=long_description,
    packages=['pycope'],
 )
