e76117b5f84ef3146e93571d1f8088b97607b496
Jake Waffle <jacqueswafel@gmail.com>
2019-12-06 22:51:22 -0800

    Added the README and other info to the setup file. Also configured Twine for deployment.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+import pathlib
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
-   name='pycope',
-   version='1.0',
-   license="Apache2",
-   description='A Context-Oriented Programming Extension for Python.',
-   author='Jacob Waffle',
-   author_email='thejakewaffle@gmail.com',
-   url="http://www.github.com/pycope",
-   packages=['pycope'],
+    name='pycope',
+    version='0.1.0',
+    license="Apache2",
+    description='A Context-Oriented Programming Extension for Python.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Jacob Waffle',
+    author_email='thejakewaffle@gmail.com',
+    url="http://www.github.com/pycope",
+    packages=['pycope'],
+    install_requires=['immutables'],
 )
